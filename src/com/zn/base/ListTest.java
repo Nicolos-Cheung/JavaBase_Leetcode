0a6f74eabe473d82b04700261ab970990ab2b43d
Nicolos-Cheung <349719588@qq.com>
2018-01-08 20:51:16 +0800

    update

--- src/com/zn/base/ListTest.java
+++ src/com/zn/base/ListTest.java
@@ -2,8 +2,12 @@ package com.zn.base;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ListTest {
@@ -12,18 +16,27 @@ public class ListTest {
 		// TODO Auto-generated method stub
 //		testIter();
 		
-		List l = new ArrayList();
-		l.add(1);
-		l.add(2);
-		l.add(3);
-		l.add(4);
-		l.add(5);
-		List subList = l.subList(0,5);
-		System.out.println(subList.size());
-		System.out.println(subList.get(4));
+		List<Map<String,String>> l = new ArrayList<>();
+		Map<String,String> h1 = new HashMap<String, String>();
+		h1.put("reviewTime", "2018-01-02");
+		Map<String,String> h2 = new HashMap<String, String>();
+		h2.put("reviewTime", "2017-04-02");
+		Map<String,String> h3 = new HashMap<String, String>();
+		h3.put("reviewTime", "2018-09-02");
+		Map<String,String> h4 = new HashMap<String, String>();
+		h4.put("reviewTime", "2017-09-12");
+		Map<String,String> h5 = new HashMap<String, String>();
+		 l.add(h1);
+		 l.add(h2);
+		 l.add(h3);
+		 l.add(h4);
+		 
+		Collections.sort(l,new MapComparator());
 		
-		
+		l.stream().forEach((s)->System.out.println(s.get("reviewTime")));
 	}
+	
+
 
 	public static void testIter() {
 
@@ -42,4 +55,16 @@ public class ListTest {
 	
 	
 
+}
+
+class MapComparator implements Comparator<Map<String ,String>>{
+
+	@Override
+	public int compare(Map<String ,String> o1, Map<String,String> o2) {
+		// TODO Auto-generated method stub
+		
+		return o1.get("reviewTime").compareTo(o2.get("reviewTime"));
+	}
+	
+	
 }
